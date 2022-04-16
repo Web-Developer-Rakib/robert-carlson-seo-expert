4ab42edf7429e91ae3900f758589d78021fe061f
WebDeveloperApolo <rakibodesk7@gmail.com>
2022-04-17 00:58:04 +0600

    Header created

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
+import Logo from "../../Images/logo.png";
+import "./Header.css";
 
 const Header = () => {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    <Navbar
+      collapseOnSelect
+      expand="lg"
+      className="nav-color fixed-top"
+      variant="light"
+    >
       <Container>
         <Navbar.Brand href="#home">
-          <img src="" alt="" />
+          <img src={Logo} alt="" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
@@ -18,6 +25,7 @@ const Header = () => {
           </Nav>
           <Nav>
             <Nav.Link href="#deets">Login</Nav.Link>
+            <Nav.Link href="#deets">Register</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
