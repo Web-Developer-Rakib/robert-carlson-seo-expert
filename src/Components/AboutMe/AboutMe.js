d52492296f312f16f3785e510d425f105b54ddcf
WebDeveloperApolo <rakibodesk7@gmail.com>
2022-04-18 11:10:01 +0600

    About section done

--- src/Components/AboutMe/AboutMe.js
+++ src/Components/AboutMe/AboutMe.js
@@ -1,10 +1,31 @@
 import React from "react";
+import RakibImg from "../../Images/rakib.jpeg";
+import "./AboutMe.css";
 
 const AboutMe = () => {
   return (
-    <div>
-      <h1>This is about me page</h1>
-    </div>
+    <section className="about-me-section mb-5">
+      <div className="d-flex flex-column flex-lg-row align-items-center flex-wrap container w-100">
+        <div className="w-50">
+          <img src={RakibImg} alt="" className="rakib-img" />
+        </div>
+        <div className="w-50">
+          <h1>About me</h1>
+          <p>
+            I am Rakibul Hassan. I have a big dream to become a successful
+            programmer/web developer. I am working hard to reach my goal. I have
+            sacrificed too many favorite things for my current goal. I was a
+            project/marketing manager of a software development company. There
+            was a big developer community. Since then, I fall in love with
+            programming. And decided to learn programming. So, I quite my job
+            and focus to learning. Now my dream is coming true.
+            <br />
+            <br />
+            <b>"I don't know, Why I am doing it. But, I will do it. :)"</b>
+          </p>
+        </div>
+      </div>
+    </section>
   );
 };
 
